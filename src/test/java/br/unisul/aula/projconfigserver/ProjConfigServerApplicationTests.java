ec335b9321db508414f9b0453d30d2b118169b67
Edson Lessa <edsonlessajunior@gmail.com>
2021-09-20 21:42:17 -0300

    Primeiro commit

---
+++ src/test/java/br/unisul/aula/projconfigserver/ProjConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unisul.aula.projconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
